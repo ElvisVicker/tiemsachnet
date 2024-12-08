76176e82d2c678c8cfa1ca4328164a4825179529
ElvisVicker <tnt963852741321@gmail.com>
2024-12-08 13:35:06 +0700

    Update dashboard

--- tiemsach/Controllers/DashboardController.cs
+++ tiemsach/Controllers/DashboardController.cs
@@ -1,23 +1,82 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using tiemsach.Data;
 using tiemsach.Models;
+using tiemsach.ViewModels;
+using static NuGet.Packaging.PackagingConstants;
+
 
 namespace tiemsach.Controllers
 {
     public class DashboardController : Controller
     {
-        private readonly ILogger<DashboardController> _logger;
+        //private readonly ILogger<DashboardController> _logger;
       
-        public DashboardController(ILogger<DashboardController> logger)
+        //public DashboardController(ILogger<DashboardController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+
+
+
+
+
+
+        private readonly TiemsachContext _context;
+        public DashboardController(TiemsachContext context)
         {
-            _logger = logger;
+            _context = context;
+
         }
+
+
+
+
 
         public IActionResult Index()
         {
 
+
             ViewData["Layout"] = "_LayoutAdmin";
-            return View();
+
+
+
+
+
+            var kh = _context.Khachhangs.ToList();
+            var nv = _context.Nhanviens.ToList();
+            var s = _context.Saches.ToList();
+            var ctpx = _context.Chitietphieuxuats.ToList();
+    
+            
+      
+
+
+            var TotalProfit = ctpx.Sum(ct => ct.Soluong * ct.Giaxuat) == null ? 0.0 : ctpx.Sum(ct => ct.Soluong * ct.Giaxuat);
+          
+      
+
+
+          
+            var TotalKH = kh.Count;
+            var TotalNV = nv.Count;
+            var TotalSach = s.Count;
+
+
+            var viewModel = new DashboardVM
+            {
+                TotalKH= TotalKH,
+                TotalNV = TotalNV,
+                TotalSach = TotalSach,
+                TotalProfit = TotalProfit
+            };
+
+
+
+
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
