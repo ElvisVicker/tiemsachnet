a8edf78aaecb35b2738f0a7083eef26d7e5c750f
ngtam0602 <132118564+ngtam0602@users.noreply.github.com>
2024-12-10 22:31:34 +0700

    Add files via upload

--- tiemsach/Controllers/SachesController.cs
+++ tiemsach/Controllers/SachesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using tiemsach.Data;
+using tiemsach.ViewModels;
 
 namespace tiemsach.Controllers
 {
@@ -22,35 +23,17 @@ namespace tiemsach.Controllers
         // GET: Saches
         public async Task<IActionResult> Index()
         {
+            ViewData["Layout"] = "_LayoutAdmin";
             var tiemsachContext = _context.Saches.Include(s => s.Loaisach).Include(s => s.Tacgia);
             return View(await tiemsachContext.ToListAsync());
-        }
-
-        // GET: Saches/Details/5
-        public async Task<IActionResult> Details(long? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var sach = await _context.Saches
-                .Include(s => s.Loaisach)
-                .Include(s => s.Tacgia)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (sach == null)
-            {
-                return NotFound();
-            }
-
-            return View(sach);
         }
 
         // GET: Saches/Create
         public IActionResult Create()
         {
-            ViewData["LoaisachId"] = new SelectList(_context.Loaisaches, "Id", "Ten");
-            ViewData["TacgiaId"] = new SelectList(_context.Tacgia, "Id", "Ten");
+            ViewData["Layout"] = "_LayoutAdmin";
+            ViewData["LoaisachId"] = new SelectList(_context.Loaisaches.Where(ls => ls.Tinhtrang == true).ToList(), "Id", "Ten");
+            ViewData["TacgiaId"] = new SelectList(_context.Tacgia.Where(tg => tg.Tinhtrang == true), "Id", "Ten");
             return View();
         }
 
@@ -59,30 +42,57 @@ namespace tiemsach.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind(",Ten,Image,Mota,TacgiaId,LoaisachId")] Sach sach)
+        public async Task<IActionResult> Create(SachVM sachModel)
         {
-            sach.Soluong = 0;
-            sach.Gianhap = 0;
-            sach.Giaxuat = 0;
-
-            sach.Tinhtrang = true;
+            ViewData["Layout"] = "_LayoutAdmin";
 
             if (ModelState.IsValid)
             {
-                _context.Add(sach);
+                string filePath = null;
+
+                if (sachModel.Image != null)
+                {
+                    var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Customer/images");
+                    if (!Directory.Exists(uploadsFolder))
+                    {
+                        Directory.CreateDirectory(uploadsFolder);
+                    }
+
+                    var uniqueFileName = Guid.NewGuid().ToString() + "_" + sachModel.Image.FileName;
+                    filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await sachModel.Image.CopyToAsync(fileStream);
+                    }
+                }
+                var sach = new Sach
+                {
+                    Ten = sachModel.Ten,
+                    Image = Path.GetFileName(filePath),
+                    TacgiaId = sachModel.TacgiaId,
+                    LoaisachId = sachModel.LoaisachId,
+                    Mota = sachModel.Mota,
+                    Tinhtrang = true,
+                    Gianhap = 0,
+                    Giaxuat = 0
+                };
+
+                _context.Saches.Add(sach);
                 await _context.SaveChangesAsync();
                 TempData["SuccessMessage"] = "Sách đã được tạo thành công!";
                 return RedirectToAction(nameof(Index));
             }
-            TempData["ErrorMessage"] = "Đã xảy ra lỗi, vui lòng kiểm tra lại!";
-            ViewData["LoaisachId"] = new SelectList(_context.Loaisaches, "Id", "Ten", sach.LoaisachId);
-            ViewData["TacgiaId"] = new SelectList(_context.Tacgia, "Id", "Ten", sach.TacgiaId);
-            return View(sach);
+            ViewData["LoaisachId"] = new SelectList(_context.Loaisaches.Where(ls => ls.Tinhtrang == true).ToList(), "Id", "Ten");
+            ViewData["TacgiaId"] = new SelectList(_context.Tacgia.Where(tg => tg.Tinhtrang == true), "Id", "Ten");
+            return View(sachModel);
         }
+
 
         // GET: Saches/Edit/5
         public async Task<IActionResult> Edit(long? id)
         {
+            ViewData["Layout"] = "_LayoutAdmin";
             if (id == null)
             {
                 return NotFound();
@@ -93,9 +103,22 @@ namespace tiemsach.Controllers
             {
                 return NotFound();
             }
-            ViewData["LoaisachId"] = new SelectList(_context.Loaisaches, "Id", "Ten", sach.LoaisachId);
-            ViewData["TacgiaId"] = new SelectList(_context.Tacgia, "Id", "Ten", sach.TacgiaId);
-            return View(sach);
+
+            var sachVM = new EditSachVM
+            {
+                Ten = sach.Ten,
+                Mota = sach.Mota,
+                Gianhap = sach.Gianhap,
+                Giaxuat = sach.Giaxuat,
+                TacgiaId = sach.TacgiaId,
+                LoaisachId = sach.LoaisachId,
+                Tinhtrang = sach.Tinhtrang,
+                ImageName = sach.Image,
+                Soluong = sach.Soluong
+            };
+            ViewData["LoaisachId"] = new SelectList(_context.Loaisaches.Where(ls => ls.Tinhtrang == true).ToList(), "Id", "Ten");
+            ViewData["TacgiaId"] = new SelectList(_context.Tacgia.Where(tg => tg.Tinhtrang == true), "Id", "Ten");
+            return View(sachVM);
         }
 
         // POST: Saches/Edit/5
@@ -103,19 +126,51 @@ namespace tiemsach.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(long id, [Bind("Id,Ten,Image,Gianhap,Giaxuat,Mota,Soluong,Tinhtrang,TacgiaId,LoaisachId")] Sach sach)
+        public async Task<IActionResult> Edit(long id, EditSachVM sachModel)
         {
-            if (id != sach.Id)
+            ViewData["Layout"] = "_LayoutAdmin";
+            Console.WriteLine("hello");
+            ModelState.Clear();
+            Console.WriteLine(id);
+            var sach = await _context.Saches.FindAsync(id);
+            if (sach == null)
             {
                 return NotFound();
             }
-
 
             if (ModelState.IsValid)
             {
                 try
                 {
-                    _context.Add(sach); // Thêm đối tượng vào context
+                    string filePath = null;
+
+                    if (sachModel.Image != null)
+                    {
+                        var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Customer/images");
+                        if (!Directory.Exists(uploadsFolder))
+                        {
+                            Directory.CreateDirectory(uploadsFolder);
+                        }
+
+                        var uniqueFileName = Guid.NewGuid().ToString() + "_" + sachModel.Image.FileName;
+                        filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await sachModel.Image.CopyToAsync(fileStream);
+                        }
+                        sach.Image = Path.GetFileName(filePath);
+                    }
+                    sach.Ten = sachModel.Ten;
+                    sach.Mota = sachModel.Mota;
+                    sach.Gianhap = sachModel.Gianhap;
+                    sach.Giaxuat = sachModel.Giaxuat;
+                    sach.TacgiaId = sachModel.TacgiaId;
+                    sach.LoaisachId = sachModel.LoaisachId;
+                    sach.Tinhtrang = sachModel.Tinhtrang;
+                    sach.Soluong = sachModel.Soluong;
+
+                    _context.Update(sach); // Thêm đối tượng vào context
                     await _context.SaveChangesAsync(); // Lưu vào cơ sở dữ liệu
                     return RedirectToAction(nameof(Index)); // Chuyển hướng về trang danh sách
                 }
@@ -125,14 +180,15 @@ namespace tiemsach.Controllers
                     ModelState.AddModelError("", "Không thể lưu dữ liệu. Lỗi: " + ex.Message);
                 }
             }
-            ViewData["LoaisachId"] = new SelectList(_context.Loaisaches, "Id", "Ten", sach.LoaisachId);
-            ViewData["TacgiaId"] = new SelectList(_context.Tacgia, "Id", "Ten", sach.TacgiaId);
+            ViewData["LoaisachId"] = new SelectList(_context.Loaisaches.Where(ls => ls.Tinhtrang == true).ToList(), "Id", "Ten");
+            ViewData["TacgiaId"] = new SelectList(_context.Tacgia.Where(tg => tg.Tinhtrang == true), "Id", "Ten");
             return View(sach);
         }
 
         // GET: Saches/Delete/5
         public async Task<IActionResult> Delete(long? id)
         {
+            ViewData["Layout"] = "_LayoutAdmin";
             if (id == null)
             {
                 return NotFound();
@@ -155,10 +211,22 @@ namespace tiemsach.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(long id)
         {
+            ViewData["Layout"] = "_LayoutAdmin";
             var sach = await _context.Saches.FindAsync(id);
             if (sach != null)
             {
-                _context.Saches.Remove(sach);
+                try
+                {
+                    sach.Tinhtrang = false;
+                    _context.Update(sach); // Thêm đối tượng vào context
+                    await _context.SaveChangesAsync(); // Lưu vào cơ sở dữ liệu
+                    return RedirectToAction(nameof(Index)); // Chuyển hướng về trang danh sách
+                }
+                catch (Exception ex)
+                {
+                    // Xử lý lỗi lưu trữ nếu có
+                    ModelState.AddModelError("", "Không thể lưu dữ liệu. Lỗi: " + ex.Message);
+                }
             }
 
             await _context.SaveChangesAsync();
