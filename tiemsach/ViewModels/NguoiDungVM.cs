d927cbd8082d41b6f89b699c80179438e869ad82
ChuViaD <131736749+ChuViaD@users.noreply.github.com>
2024-12-10 23:38:14 +0700

    Nguoi Dung/Quyen

---
+++ tiemsach/ViewModels/NguoiDungVM.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tiemsach.ViewModels
+{
+    public class NguoiDungVM
+    {
+        [Required(ErrorMessage = "Nhập đầy đủ thông tin")]
+        public long Id { get; set; }
+        [Required(ErrorMessage = "Nhập đầy đủ thông tin")]
+        public long QuyenId { get; set; }
+        [Required(ErrorMessage = "Nhập đầy đủ thông tin")]
+        public required string Hoten { get; set; }
+        public bool Gioitinh { get; set; }
+        [Required(ErrorMessage = "Nhập đầy đủ thông tin")]
+        public bool Vaitro { get; set; }
+        [Required(ErrorMessage = "Nhập đầy đủ thông tin")]
+        public string Sodienthoai { get; set; }
+        public string? Diachi { get; set; }
+        public bool Tinhtrang { get; set; }
+        [Required(ErrorMessage = "Vui lòng chọn hình ảnh.")]
+        public IFormFile? ImageFile { get; set; }
+        public string? Image { get; set; }
+        [Required(ErrorMessage = "Nhập đầy đủ thông tin")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Nhập đầy đủ thông tin")]
+        public string Password { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        }
+    }
+    
